548aa86df3f616331db0199afa2c562e920eff4e
Gravy Boat <gravy@thatgravyboat.tech>
2026-01-05 18:01:54 -0330

    chore: add maven publishing

--- cats4j/build.gradle.kts
+++ cats4j/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
+    id("maven-publish")
     id("java")
 }
 
 group = "me.owdding"
-version = "1.0.0"
+version = "1.0.0-beta.1"
 
 java {
     withSourcesJar()
@@ -12,5 +13,24 @@ java {
 tasks {
     compileJava {
         options.encoding = "UTF-8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            setUrl("https://maven.teamresourceful.com/repository/thatgravyboat/")
+
+            credentials {
+                username = System.getenv("MAVEN_USER") ?: providers.gradleProperty("maven_username").orNull
+                password = System.getenv("MAVEN_PASS") ?: providers.gradleProperty("maven_password").orNull
+            }
+        }
     }
 }
